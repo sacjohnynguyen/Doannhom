94711af9c59245fbc9d88de3d8aa8cbe40491f9f
sacjohnynguyen <quangsac3@gmail.com>
2026-03-29 13:27:59 +0700

    Add files via upload

---
+++ ThaySanh.java
@@ -0,0 +1,24 @@
+
+package thaysanh;
+import java.util.*;
+public class ThaySanh {
+    int []a;
+    void sinhMang(int n){
+        Random rd = new Random();
+        a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = rd.nextInt(1,10);
+        }
+    }
+    void in(String t){
+        System.out.println("\n"+ t);
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        ThaySanh mang = new ThaySanh();
+        mang.sinhMang(10);
+        mang.in("Hien thi noi dung");
+    }
+}
