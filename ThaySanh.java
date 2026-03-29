ce556d19dba83042a20dbd93b38e72a9c8daafeb
nhdeptrai1 <nngocsang098@gmail.com>
2026-03-29 14:15:45 +0700

    Add files via upload

---
+++ ThaySanh.java
@@ -0,0 +1,39 @@
+
+package thaysanh;
+import java.util.*;
+public class ThaySanh {
+    int []a;
+    void sinhMang(int n){
+        Random rd = new Random();
+        a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = rd.nextInt(1,10);
+        }
+    }
+    void in(String t){
+        System.out.println("\n"+ t);
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+ void timMax() {
+        if (a == null || a.length == 0) {
+            System.out.println("Mang rong!");
+            return;
+        }
+        
+        int max = a[0]; // Giả sử phần tử đầu tiên là lớn nhất
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i]; // Cập nhật lại max nếu tìm thấy số lớn hơn
+            }
+        }
+        System.out.println("GGTLN trong mang la " + max);
+    }
+    public static void main(String[] args) {
+        ThaySanh mang = new ThaySanh();
+        mang.sinhMang(10);
+        mang.in("Hien thi noi dung");
+       mang.timMax(); 
+    }
+}
